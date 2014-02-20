3fd20fce78d9b8c8f35c000027448e37d3d6a7d9
Esteban <stephendavidboyd@gmail.com>
2014-02-20 15:23:17 -0800

    mucking around in exercise 1

---
+++ kern/startup/ducke.c
@@ -0,0 +1,18 @@
+#include <types.h>
+#include <lib.h>
+void ducke(){
+	kprintf("                        __\n");
+	kprintf("                      /` ,\\__\n");
+	kprintf("                     |    ).-'\n");
+	kprintf("                    / .--'\n");
+	kprintf("                   / /\n");
+	kprintf("     ,      _.==''`  \\\n");
+	kprintf("   .'(  _.='         |\n");
+	kprintf("  {   ``  _.='       |\n");
+	kprintf("   {    \\`     ;    /\n");
+	kprintf("    `.   `'=..'  .='\n");
+	kprintf("      `=._    .='\n");
+	kprintf("        '-`\\\\`__\n");
+	kprintf("            `-._{\n");
+	kprintf("\n");
+}
